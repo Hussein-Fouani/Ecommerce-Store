a95f5811db54d473e2c83b6a30586f092635bf0b
Hussein <social.101.acc@gmail.com>
2024-01-14 10:37:04 +0200

    Add UserAuthService dependency to UserService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserAuthService } from './user-auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -7,10 +8,31 @@ import { Injectable } from '@angular/core';
 export class UserService {
   url_base: string = '/api/v1/auth';
   requestheader = new HttpHeaders({
-    "No-Auth":"True"
+    'No-Auth': 'True',
   });
-  constructor(private httpclient: HttpClient) {}
+  constructor(
+    private httpclient: HttpClient,
+    private userservice: UserAuthService
+  ) {}
+
   public login(loginData: String) {
-    return this.httpclient.post(this.url_base + '/signin', loginData,{headers:this.requestheader});
+    return this.httpclient.post(this.url_base + '/signin', loginData, {
+      headers: this.requestheader,
+    });
+  }
+  public roleMatch(allowedroles: any): boolean {
+    let isMatched = false;
+    const userRoles: any = this.userservice.getRoles();
+    if (userRoles != null && userRoles) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowedroles.length; j++) {
+          if (userRoles[i].role == allowedroles[j]) {
+            isMatched = true;
+            return isMatched;
+          }
+        }
+      }
+    }
+     return (isMatched = false);
   }
 }
