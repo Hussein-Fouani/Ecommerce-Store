f026e81aca489d57ffcb9c14396c8929a28f76ae
Hussein <social.101.acc@gmail.com>
2024-01-14 11:39:59 +0200

    Refactor AuthGuard to implement CanActivate interface

--- src/app/_auth/auth.guard.ts
+++ src/app/_auth/auth.guard.ts
@@ -1,5 +1,34 @@
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { UserAuthService } from '../services/user-auth.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private userauth: UserAuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (this.userauth.getToken() !== null) {
+      const role =route.data['roles'] as Array<string>;
+      if(role){
+        
+      }
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
