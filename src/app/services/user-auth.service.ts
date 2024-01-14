736f0ca24f4f003f7e60b4ff9715af80c6201bc7
Hussein <social.101.acc@gmail.com>
2024-01-14 10:02:48 +0200

    Add UserAuthService class with methods for role management and authentication

---
+++ src/app/services/user-auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserAuthService {
+  constructor() {}
+
+  public setRoles(roles: []) {
+    localStorage.setItem('roles', JSON.stringify(roles));
+  }
+  public getRoles(): [] {
+    return JSON.parse(localStorage.getItem('roles') as string);
+  }
+  public setToken(jwttoken: string) {
+    localStorage.setItem('jwttoken', jwttoken);
+  }
+  public getToken(): string {
+    return localStorage.getItem('jwttoken') as string;
+  } 
+  public clear(){
+    localStorage.clear();
+  }
+  public isAuthenticated() {
+    return this.getRoles() && this.getToken();
+  }
+}
