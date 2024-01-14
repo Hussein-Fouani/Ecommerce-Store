24d7c11aa2d5b0397dd0ceaab9dfe906be0bafa5
Hussein <social.101.acc@gmail.com>
2024-01-14 19:41:24 +0200

    Add new routes for admin and user components, and add new product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,17 @@ import { AdminComponent } from './admin/admin.component';
 import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { AddNewProductComponent } from './add-new-product/add-new-product.component';
+import { AuthGuard } from './_auth/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'admin', component: AdminComponent },
-  { path: 'user', component: UserComponent },
+  { path: 'admin', component: AdminComponent,canActivate:[AuthGuard],data:{role:['ADMIN']}  },
+  { path: 'user', component: UserComponent,canActivate:[AuthGuard],data:{role:['USER']}  },
   { path: 'forbidden', component: ForbiddenComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {path:'addnewproduct',component:AddNewProductComponent}
 ];
 
 @NgModule({
