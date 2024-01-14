0b57481b8d412e71250ac20e6412efc8de2c4ce4
Hussein <social.101.acc@gmail.com>
2024-01-14 14:25:32 +0200

    Add UserService and implement forUsers method in UserComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-export class UserComponent {
-
+export class UserComponent implements OnInit {
+  message: any;
+  constructor(private userservice: UserService) {}
+  ngOnInit(): void {
+    this.forUsers();
+  }
+  forUsers() {
+    this.userservice.forUser().subscribe((subs) => {
+      this.message = subs;
+    });
+  }
 }
