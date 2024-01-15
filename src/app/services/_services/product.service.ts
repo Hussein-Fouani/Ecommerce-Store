6ad25aa5ed547425f15f39240900d8f54da521ab
Hussein <social.101.acc@gmail.com>
2024-01-15 16:53:04 +0200

    Refactor ProductService to include getProducts and setProducts methods

--- src/app/services/_services/product.service.ts
+++ src/app/services/_services/product.service.ts
@@ -1,16 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../../model/product.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  private baseurl="http://localhost:9090/api/v1/addNewProduct"
+  private baseurl = 'http://localhost:9090/api/v1/';
 
-  constructor(private httpclient:HttpClient) { }
+  private products: Product[] = [];
 
-  public addProduct(product :Product){
-   return this.httpclient.post<Product>(this.baseurl,product);
+  constructor(private httpClient: HttpClient) {}
+
+  public getProducts(): Observable<Product[]> {
+    const url = `${this.baseurl}/getProducts`; // Adjust the URL to your backend API
+    return this.httpClient.get<Product[]>(url);
+  }
+
+  public setProducts(products: Product[]): void {
+    this.products = products;
+  }
+
+  public addProduct(product: FormData) {
+    return this.httpClient.post<Product>(this.baseurl, product);
   }
 }
