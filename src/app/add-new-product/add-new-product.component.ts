f86acd4df054664ed6441c9490573a67b41475f8
Hussein <social.101.acc@gmail.com>
2024-01-15 10:44:30 +0200

    Add new product component with form and service integration

--- src/app/add-new-product/add-new-product.component.ts
+++ src/app/add-new-product/add-new-product.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { NgForm } from '@angular/forms';
+import { ProductService } from '../services/_services/product.service';
 
 @Component({
   selector: 'app-add-new-product',
   templateUrl: './add-new-product.component.html',
-  styleUrl: './add-new-product.component.css'
+  styleUrl: './add-new-product.component.css',
 })
 export class AddNewProductComponent {
+  product: Product = {
+    productname: '',
+    productdescription: '',
+    productprice: 0,
+    discountprice: 0,
+  };
 
+  constructor(private productservice:ProductService) {}
+  addProduct(productform: NgForm) {
+    this.productservice.addProduct(this.product).subscribe((sub)=>{
+      
+    })
+  }
 }
