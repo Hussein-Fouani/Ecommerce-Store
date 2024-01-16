7e31c9c584efa5f0fcfeb1eb16768909b6999783
Hussein <social.101.acc@gmail.com>
2024-01-16 17:07:37 +0200

    Refactor showproducts-details.component.ts

--- src/app/showproducts-details/showproducts-details.component.ts
+++ src/app/showproducts-details/showproducts-details.component.ts
@@ -5,17 +5,24 @@ import { Product } from '../model/product.model';
 @Component({
   selector: 'app-showproducts-details',
   templateUrl: './showproducts-details.component.html',
-  styleUrl: './showproducts-details.component.css'
+  styleUrl: './showproducts-details.component.css',
 })
 export class ShowproductsDetailsComponent implements OnInit {
-  constructor(private product:ProductService){}
+  constructor(private product: ProductService) {}
+  productDetails: Product[] = [];
+  displayedColumns: string[] = [
+    'productId',
+    'productname',
+    'productdescription',
+    'productprice',
+    'discountprice',
+  ];
   ngOnInit(): void {
     this.getProducts();
   }
-  public getProducts(){
-    this.product.getAllProducts().subscribe((sub:Product[])=>{
-
-    })
+  public getProducts() {
+    this.product.getAllProducts().subscribe((sub: Product[]) => {
+      this.productDetails = sub;
+    });
   }
-
 }
