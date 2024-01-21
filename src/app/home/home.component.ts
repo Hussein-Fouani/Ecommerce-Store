7f77ae89feb1e52bead23f373fcaf0dc6ebf7304
Hussein <41930483@students.liu.edu.lb>
2024-01-21 23:36:00 +0200

    Add product details and image processing functionality to HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/_services/product.service';
+import { Product } from '../model/product.model';
+import { ImageProcessingService } from '../services/image-processing.service';
+import { tap } from 'rxjs';
+import { Init } from 'v8';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  productDetails:any;
+  constructor(
+    private productservice: ProductService,
+    private imageProcessingService: ImageProcessingService
+  ) {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts() {
+    this.productservice
+      .getAllProducts()
+      .pipe(
+        tap((products: Product[]) => {
+          products.forEach((prod: Product) => {
+            this.imageProcessingService.createImages(prod);
+          });
+        })
+      )
+      .subscribe((sub: Product[]) => {
+        this.productDetails = sub;
+      });
+  }
 }
