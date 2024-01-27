0d55fee222d8894fa702e39aab70d5866ef54ed9
Hussein <41930483@students.liu.edu.lb>
2024-01-27 11:11:23 +0200

    Add product model and route activation to product view component

--- src/app/productview/productview.component.ts
+++ src/app/productview/productview.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-productview',
   templateUrl: './productview.component.html',
-  styleUrl: './productview.component.css'
+  styleUrl: './productview.component.css',
 })
-export class ProductviewComponent {
+export class ProductviewComponent implements OnInit {
+  product: Product;
 
+  constructor(private activatedrouter:ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedrouter.snapshot.data['product'];
+    
+  }
 }
